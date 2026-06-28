f1e1a9167c8e1dbb3a69b0bea672f77ce3a25621
afzal <write1afzal@gmail.com>
2026-06-28 14:40:35 +0530

    Error fixed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,13 @@ import cors from "cors";
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { config } from "./config/config.js";
+
+// Deploye section
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 
@@ -24,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://snitch-project-zuqv.onrender.com"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }))
